75df06bd7fbd9f86898e9fbe23119cc71e1b1386
Phapoom Saksri <mind@blueskychan.dev>
2023-12-28 01:00:56 +0700

    meta updates :3

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import About from "~/components/About"
 import Avatar from "~/components/Avatar"
 import Profile from "~/components/Profile"
 import SocialLinks from "~/components/SocialLinks"
+import Head from "next/Head"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -11,6 +12,11 @@ export default function Home() {
     <main
       className={`flex min-h-screen items-center justify-center ${inter.className}`}
     >
+       <Head>
+        <title>About blueskychan_ :3</title>
+        <meta property="og:title" content="Hello, World! >3" key="title" />
+        <meta property="og:description" content="About some stupid person call blueskychan, aka Fuse/Mind :3" />
+      </Head>
       <div className="backdrop-blur-md bg-gray-700/50 rounded-lg p-2">
         <div className="flex flex-row space-x-3 p-4 items-center ">
           <Avatar />
