033e73008f51fecda7717ca8334ee013dc70e0d5
mac <bluestardotos@gmail.com>
2024-04-16 02:00:13 +0700

    Colorful update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,8 @@
 const Profile = () => {
   return (
-    <div className="text-gray-300 text-lg">
+    <div className="text-lg">
       <p className="space-x-1">
-        <span className="text-gray-100 text-lg font-bold md:text-xl">
+        <span className="text-lg font-bold md:text-xl">
           blueskychan_
         </span>
         <span className="font-extralight text-base md:text-xl">
