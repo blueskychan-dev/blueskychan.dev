753a29cca18e2197746368612d73d73c6f1f586b
mac <bluestardotos@gmail.com>
2024-04-16 02:00:13 +0700

    Colorful update

--- src/components/PageLink.jsx
+++ src/components/PageLink.jsx
@@ -2,7 +2,7 @@ const PageLink = ({ to, text }) => {
   return (
     <a
       href={to}
-      className="p-1 bg-blue-600/20 transition-colors hover:bg-blue-700 font-bold rounded-lg border-2 border-blue/50"
+      className="px-2.5 py-0.5 bg-blue-600/20 transition-colors hover:bg-blue-700 font-bold rounded-lg border border-blue/50"
     >
       {text}
     </a>
