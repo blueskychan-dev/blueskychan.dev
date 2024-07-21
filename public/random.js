f4ab96e1f76c1e841ebd7d834ebaafb2565f4619
blue <canikissfemboy@gmail.com>
2024-07-21 20:51:31 +0700

    get real

--- public/random.js
+++ public/random.js
@@ -1,3 +1,7 @@
-console.log("What are you doing here? ;-;");
-console.log("Tips: If you add stright=true to the URL, the font will change to Roboto, easier to read :3");
-console.log("Background Sauce: https://www.pixiv.net/en/artworks/113909210 (I like that artwork but be careful with thier profile, they include blue archive NSFW content and I didn't want to see it ;-;)");
+console.log("What are you doing here? ;-;")
+console.log(
+  "Tips: If you add stright=true to the URL, the font will change to Roboto, easier to read :3",
+)
+console.log(
+  "Background Sauce: https://www.pixiv.net/en/artworks/113909210 (I like that artwork but be careful with thier profile, they include blue archive NSFW content and I didn't want to see it ;-;)",
+)
