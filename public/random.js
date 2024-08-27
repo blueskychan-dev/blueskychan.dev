1608d09d66fc4bd6aec519718f515a79d62d8568
Phapoom Saksri <mind@blueskychan.dev>
2024-08-27 22:00:50 +0700

    idk

--- public/random.js
+++ public/random.js
@@ -3,5 +3,5 @@ console.log(
   "Tips: If you add stright=true to the URL, the font will change to Roboto, easier to read :3",
 )
 console.log(
-  "Background Sauce: https://www.pixiv.net/en/artworks/113909210 (I like that artwork but be careful with thier profile, they include blue archive NSFW content and I didn't want to see it ;-;)",
+  "Background Sauce: https://www.pixiv.net/en/artworks/120913376",
 )
