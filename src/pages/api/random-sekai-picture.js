6469cb42c27951e28dd6e7b1ddfb28638d9b08d0
Phapoom Saksri <mind@blueskychan.dev>
2024-12-08 21:12:05 +0700

    From bluestar-b requested

--- src/pages/api/random-sekai-picture.js
+++ src/pages/api/random-sekai-picture.js
@@ -2,8 +2,19 @@ import path from "path";
 import fs from "fs";
 
 export default function handler(req, res) {
+    if (req.method !== "GET") {
+        res.status(405).json({ message: "Method Not Allowed" });
+        return;
+    }
+
     const random = Math.floor(Math.random() * 3) + 1; // Generate random number 1-3
     const imagePath = path.join(process.cwd(), "public", "sekai", `${random}.jpeg`);
+
+    // Check if the image file exists
+    if (!fs.existsSync(imagePath)) {
+        res.status(404).json({ message: "Image Not Found" });
+        return;
+    }
 
     // Read the image file from the filesystem
     const image = fs.readFileSync(imagePath);
