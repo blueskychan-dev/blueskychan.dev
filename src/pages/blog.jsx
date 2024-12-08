b0fa49547002958af59bc4b6ccc380ac1b1047e0
Phapoom Saksri <mind@blueskychan.dev>
2024-12-08 19:34:57 +0700

    almost done...

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -6,15 +6,17 @@ import Head from "next/head"
 export async function getStaticProps() {
   const files = fs.readdirSync("posts")
 
-  const posts = files.map((fileName) => {
-    const slug = fileName.replace(".md", "")
-    const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8")
-    const { data: frontmatter } = matter(readFile)
-    return {
-      slug,
-      frontmatter,
-    }
-  })
+  const posts = files
+    .filter((fileName) => !fileName.endsWith("-th.md"))
+    .map((fileName) => {
+      const slug = fileName.replace(".md", "")
+      const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8")
+      const { data: frontmatter } = matter(readFile)
+      return {
+        slug,
+        frontmatter,
+      }
+    })
 
   return {
     props: {
